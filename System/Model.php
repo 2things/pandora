d9b5b68849a4abce07dc42afce919254cb4ea6e8
gevorg <gevorg@gevorg-PC>
2014-10-02 17:12:30 +0400

    class for models

--- System/Model.php
+++ System/Model.php
@@ -1,1 +1,54 @@
 <?php
+
+namespace System;
+
+/**
+ * Model class
+ * 
+ * @author Gevorg Makaryan <makaryan.gevorg@gmail.com>
+ */
+class Model
+{
+    /**
+     *
+     * @var System\Config
+     */
+    private $config;
+    
+    /**
+     *
+     * @var string
+     */
+    private $appModelNamespace = '\\App\\Model\\';
+    
+    /**
+     * Constructor of the class
+     */
+    public function __constructor()
+    {
+        $this->config = new \System\Config();
+    }
+    
+    /**
+     * Gets the requested model from App\Model
+     * 
+     * @param string $model
+     * 
+     * @return object 
+     */
+    public function get($model)
+    {
+        $modelFull = '\\App\\Model\\' . $model;
+        return new $modelFull($this->config);
+    }
+    
+    /**
+     * Gets the configuration class
+     * 
+     * @return \System\Config
+     */
+    public function getConfig()
+    {
+        return $this->config;
+    }
+}
