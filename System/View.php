76b2b847930e4ccb1bdde4b5c958f5dbe9b988e7
gevorg <gevorg@gevorg-PC>
2014-10-02 17:12:40 +0400

    class for views

--- System/View.php
+++ System/View.php
@@ -1,1 +1,39 @@
 <?php
+
+namespace System;
+
+/**
+ * View class
+ * 
+ * @author Gevorg Makaryan <makaryan.gevorg@gmail.com>
+ */
+class View
+{
+    /**
+     *
+     * @var \Smarty
+     */
+    protected $smarty;
+    
+    /**
+     * Contructor of the class
+     */
+    public function __construct()
+    {
+        $this->smarty = new Smarty();
+        
+        $this->smarty->template_dir = PATH_VIEW . '/';
+        $this->smarty->compile_dir  = PATH_ROOT . '/../tmp/Smarty/compile/';
+        $this->smarty->cache_dir    = PATH_ROOT . '/../tmp/Smarty/cache/';
+    }
+    
+    /**
+     * Gets the view object
+     * 
+     * @return \Smarty
+     */
+    public function getView()
+    {
+        return $this->smarty;
+    }
+}
