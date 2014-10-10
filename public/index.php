94b498ac1d03e1cab8aa52bb6db3da04fab17ce1
gevorg <gevorg@gevorg-PC>
2014-10-10 18:30:02 +0400

    added constants to store some system paths

--- public/index.php
+++ public/index.php
@@ -8,6 +8,9 @@ $root = dirname(__DIR__);
 
 define('PATH_ROOT', $root);
 define('PATH_SYSTEM', PATH_ROOT . '/System');
+define('PATH_TRANSLATION', PATH_ROOT . '/Translation');
+define('PATH_LIBRARY', PATH_ROOT . '/Library');
+define('PATH_HELPER', PATH_ROOT . '/Helper');
 define('PATH_CONTROLLER', PATH_ROOT . '/App/Controller');
 define('PATH_MODEL', PATH_ROOT . '/App/Model');
 define('PATH_VIEW', PATH_ROOT . '/App/View');
