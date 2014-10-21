20d5cadf90f19af4020ca2bac8f5648c33c75d64
gevorg <gevorg@gevorg-PC>
2014-10-21 12:28:19 +0400

    display error pages

--- App/Controller/ErrorController.php
+++ App/Controller/ErrorController.php
@@ -7,14 +7,14 @@ class errorController extends System\HttpFrontController
 {
     public function error404Action()
     {
-        echo '404 not found';
-        exit;
+        $this->getView()->setVariable('isAuthorized', \App\Model\User::isAuthorized());
+        $this->getView()->getSmarty()->display('Error/404.tpl');
     }
     
     public function error500Action()
     {
-        echo '500 server internal error';
-        exit;
+        $this->getView()->setVariable('isAuthorized', \App\Model\User::isAuthorized());
+        $this->getView()->getSmarty()->display('Error/500.tpl');
     }
 }
 
