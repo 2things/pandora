bade533f79ab4e3a857c08a145d491e44955f72a
gevorg <gevorg@gevorg-PC>
2014-10-25 16:56:31 +0400

    working with profiles

--- eplanner.sql
+++ eplanner.sql
@@ -10,7 +10,7 @@ Target Server Type    : MYSQL
 Target Server Version : 50611
 File Encoding         : 65001
 
-Date: 2014-10-23 17:05:15
+Date: 2014-10-25 16:55:02
 */
 
 SET FOREIGN_KEY_CHECKS=0;
@@ -36,6 +36,40 @@ CREATE TABLE `goal` (
 -- ----------------------------
 -- Records of goal
 -- ----------------------------
+
+-- ----------------------------
+-- Table structure for `profile`
+-- ----------------------------
+DROP TABLE IF EXISTS `profile`;
+CREATE TABLE `profile` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `user_id` int(11) NOT NULL,
+  `avatar` varchar(255) DEFAULT NULL,
+  `name` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `about` text CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci,
+  `website_url` varchar(250) DEFAULT NULL,
+  `fb_url` varchar(250) DEFAULT NULL,
+  `twitter_url` varchar(250) DEFAULT NULL,
+  `followers` int(11) DEFAULT NULL,
+  `followings` int(11) DEFAULT NULL,
+  `wishlist` int(11) DEFAULT NULL,
+  `goals` int(11) DEFAULT NULL,
+  `likes` int(11) DEFAULT NULL,
+  `profession` int(11) DEFAULT NULL,
+  `birth_date` date DEFAULT NULL,
+  `created_date` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `profile_rating` tinyint(2) DEFAULT NULL,
+  `profile_type` varchar(30) NOT NULL DEFAULT 'individual',
+  PRIMARY KEY (`id`),
+  KEY `user_id` (`user_id`),
+  CONSTRAINT `user_id` FOREIGN KEY (`user_id`) REFERENCES `user` (`id`) ON DELETE CASCADE ON UPDATE NO ACTION
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=latin1;
+
+-- ----------------------------
+-- Records of profile
+-- ----------------------------
+INSERT INTO `profile` VALUES ('1', '9', null, null, null, null, null, null, null, null, null, null, null, null, null, '2014-10-24 14:30:15', null, 'individual');
+INSERT INTO `profile` VALUES ('2', '10', null, null, null, null, null, null, null, null, null, null, null, null, null, '2014-10-24 14:37:13', null, 'individual');
 
 -- ----------------------------
 -- Table structure for `todo`
@@ -69,7 +103,7 @@ CREATE TABLE `user` (
   PRIMARY KEY (`id`),
   UNIQUE KEY `username` (`username`),
   UNIQUE KEY `email` (`email`)
-) ENGINE=InnoDB AUTO_INCREMENT=9 DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=11 DEFAULT CHARSET=latin1;
 
 -- ----------------------------
 -- Records of user
@@ -80,3 +114,12 @@ INSERT INTO `user` VALUES ('5', 'makaryan.gevorg1@mail.ru', 'toktik2', '123123')
 INSERT INTO `user` VALUES ('6', 'makaryan.gevorg2@mail.ru', 'toktik3', 'c20ad4d76fe97759aa27a0c99bff6710');
 INSERT INTO `user` VALUES ('7', 'test.user@mail.ru', 'test', '098f6bcd4621d373cade4e832627b4f6');
 INSERT INTO `user` VALUES ('8', 'marashlyan.suren@mail.ru', 'suren', '202cb962ac59075b964b07152d234b70');
+INSERT INTO `user` VALUES ('9', 'mak.gev@mail.com', 'gevorg', '54eb9c413fd20404c0e4da1b5eb41bf6');
+INSERT INTO `user` VALUES ('10', 'ms@mail.ru', 'gevorg1', 'af4599c4ddc76d02e8114774f196c0e4');
+DROP TRIGGER IF EXISTS `create_profile`;
+DELIMITER ;;
+CREATE TRIGGER `create_profile` AFTER INSERT ON `user` FOR EACH ROW BEGIN
+INSERT INTO `profile` (user_id) VALUES (NEW.id);
+END
+;;
+DELIMITER ;
