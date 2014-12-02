612d8ad916c7006be3839da5222b765bcd2f5f51
Gevorg Makaryan <git.makaryangevorg@gmail.com>
2014-12-02 17:00:58 +0400

    added tables

--- eplanner.sql
+++ eplanner.sql
@@ -1,16 +1,16 @@
 /*
 Navicat MySQL Data Transfer
 
-Source Server         : localhost
+Source Server         : local_db
 Source Server Version : 50611
-Source Host           : localhost:3306
-Source Database       : eplanner
+Source Host           : 127.0.0.1:3306
+Source Database       : eplanner_new
 
 Target Server Type    : MYSQL
 Target Server Version : 50611
 File Encoding         : 65001
 
-Date: 2014-10-27 17:56:04
+Date: 2014-12-02 17:00:25
 */
 
 SET FOREIGN_KEY_CHECKS=0;
@@ -25,7 +25,7 @@ CREATE TABLE `goal` (
   `title` text CHARACTER SET utf8 NOT NULL,
   `description` text CHARACTER SET utf8,
   `img_attach` longblob,
-  `created_date` timestamp NULL,
+  `created_date` timestamp NULL DEFAULT NULL,
   `updated_date` timestamp NULL DEFAULT NULL ON UPDATE CURRENT_TIMESTAMP,
   `status` tinyint(3) NOT NULL DEFAULT '0',
   PRIMARY KEY (`id`),
@@ -36,6 +36,53 @@ CREATE TABLE `goal` (
 -- ----------------------------
 -- Records of goal
 -- ----------------------------
+
+-- ----------------------------
+-- Table structure for `goal_categories`
+-- ----------------------------
+DROP TABLE IF EXISTS `goal_categories`;
+CREATE TABLE `goal_categories` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(25) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=34 DEFAULT CHARSET=utf8mb4;
+
+-- ----------------------------
+-- Records of goal_categories
+-- ----------------------------
+INSERT INTO `goal_categories` VALUES ('1', 'Animals');
+INSERT INTO `goal_categories` VALUES ('2', ' Architecture');
+INSERT INTO `goal_categories` VALUES ('3', 'Art');
+INSERT INTO `goal_categories` VALUES ('4', 'Business');
+INSERT INTO `goal_categories` VALUES ('5', 'Cars & Motorcycles');
+INSERT INTO `goal_categories` VALUES ('6', 'Celebrities');
+INSERT INTO `goal_categories` VALUES ('7', 'Design');
+INSERT INTO `goal_categories` VALUES ('8', 'Education');
+INSERT INTO `goal_categories` VALUES ('9', 'Family');
+INSERT INTO `goal_categories` VALUES ('10', 'Film, Music & Books');
+INSERT INTO `goal_categories` VALUES ('11', 'Food & Drink');
+INSERT INTO `goal_categories` VALUES ('12', 'Gardening');
+INSERT INTO `goal_categories` VALUES ('13', 'Geek');
+INSERT INTO `goal_categories` VALUES ('14', 'Hair & Beauty');
+INSERT INTO `goal_categories` VALUES ('15', 'Health & Fitness');
+INSERT INTO `goal_categories` VALUES ('16', ' History');
+INSERT INTO `goal_categories` VALUES ('17', 'Holidays & Events');
+INSERT INTO `goal_categories` VALUES ('18', 'Home Decor');
+INSERT INTO `goal_categories` VALUES ('19', 'Humor');
+INSERT INTO `goal_categories` VALUES ('20', 'Illustrations & Posters');
+INSERT INTO `goal_categories` VALUES ('21', 'Kids');
+INSERT INTO `goal_categories` VALUES ('22', 'Men\'s Fashion');
+INSERT INTO `goal_categories` VALUES ('23', 'Outdoors');
+INSERT INTO `goal_categories` VALUES ('24', 'Photography');
+INSERT INTO `goal_categories` VALUES ('25', 'Products');
+INSERT INTO `goal_categories` VALUES ('26', 'Science & Nature');
+INSERT INTO `goal_categories` VALUES ('27', 'Sports');
+INSERT INTO `goal_categories` VALUES ('28', 'Tattoos');
+INSERT INTO `goal_categories` VALUES ('29', 'Technology');
+INSERT INTO `goal_categories` VALUES ('30', 'Travel');
+INSERT INTO `goal_categories` VALUES ('31', 'Weddings ');
+INSERT INTO `goal_categories` VALUES ('32', 'Women\'s Fashion');
+INSERT INTO `goal_categories` VALUES ('33', 'Other');
 
 -- ----------------------------
 -- Table structure for `profile`
@@ -72,6 +119,25 @@ INSERT INTO `profile` VALUES ('1', '9', null, null, null, null, null, null, null
 INSERT INTO `profile` VALUES ('2', '10', null, null, null, null, null, null, null, null, null, null, null, null, null, '2014-10-24 14:37:13', null, 'individual');
 
 -- ----------------------------
+-- Table structure for `relation_goals_categories`
+-- ----------------------------
+DROP TABLE IF EXISTS `relation_goals_categories`;
+CREATE TABLE `relation_goals_categories` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `category_id` int(11) NOT NULL,
+  `goal_id` int(11) NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `cat_id` (`category_id`),
+  KEY `g_id` (`goal_id`),
+  CONSTRAINT `cat_id` FOREIGN KEY (`category_id`) REFERENCES `goal_categories` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION,
+  CONSTRAINT `g_id` FOREIGN KEY (`goal_id`) REFERENCES `goal` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- ----------------------------
+-- Records of relation_goals_categories
+-- ----------------------------
+
+-- ----------------------------
 -- Table structure for `task`
 -- ----------------------------
 DROP TABLE IF EXISTS `task`;
@@ -80,7 +146,7 @@ CREATE TABLE `task` (
   `user_id` int(11) NOT NULL,
   `title` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
   `status` tinyint(2) NOT NULL DEFAULT '0',
-  `created_date` timestamp NULL,
+  `created_date` timestamp NULL DEFAULT NULL,
   `updated_date` timestamp NULL DEFAULT NULL ON UPDATE CURRENT_TIMESTAMP,
   PRIMARY KEY (`id`),
   KEY `profile` (`user_id`),
